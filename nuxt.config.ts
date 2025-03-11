ca983a3176ea8e8b3b9bca73204d58dcbbb2c85a
rangguy <ranggadwi100@gmail.com>
2025-03-11 12:57:33 +0700

    berhasil install tailwind + konekin tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/tailwind.css"],
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
     head: {
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap' }
-      ]
-    }
-  }
-})
-
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
+        },
+      ],
+    },
+  },
+});
