9267ceacc24bab9e8ad4758310acf8e6dd8f2d3f
rangguy <ranggadwi100@gmail.com>
2025-03-15 22:16:34 +0700

    konfigurasi authentikasi menggunakan sidebase-nuxt-auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth"],
   css: ["~/assets/css/tailwind.css"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
     head: {
-      title:"Crowd Funding App",
+      title: "Crowd Funding App",
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         {
@@ -19,6 +19,36 @@ export default defineNuxtConfig({
           href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
         },
       ],
+    },
+  },
+  auth: {
+    provider: {
+      type: "local",
+      endpoints: {
+        signIn: {
+          path: "/api/v1/sessions",
+          method: "post",
+        },
+        signUp: {
+          path: "/api/v1/users",
+          method: "post",
+        },
+        signOut: { path: "/api/v1/sessions", method: "delete" },
+        getSession: { path: "/api/v1/users/fetch", method: "get" },
+      },
+      token: {
+        signInResponseTokenPointer: "data.token",
+        type: "Bearer",
+        headerName: "Authorization",
+      },
+      pages: {
+        login: "/login",
+      },
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: "http://localhost:8080/",
     },
   },
 });
